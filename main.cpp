457d67dcbf2c70596b1e757efeb4451b62da0267
Toprak KILIÇ <toprakkilic23@gmail.com>
2025-04-19 13:40:11 +0300

    top bir sınıf olarak yazıldı ve düsman sınıfı eklendi

--- main.cpp
+++ main.cpp
@@ -3,51 +3,117 @@
 
 using namespace std;
 
-int main () {
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+class Ball {
+public:
+    int ball_x;
+    int ball_y;
+    int ball_speed_x;
+    int ball_speed_y;
+    int ball_radius;
 
-    cout << "Hello World" << endl;
+    Ball(int startX, int startY, int startSpeedX, int startSpeedY, int ballRadius) 
+        : ball_x(startX), ball_y(startY), ball_speed_x(startSpeedX), ball_speed_y(startSpeedY), ball_radius(ballRadius) {}
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (WindowShouldClose() == false) {
-        // W tuşuna basıldığında yukarı hareket
+    void Update(int SCREEN_WIDTH, int SCREEN_HEIGHT) {
         if (IsKeyDown(KEY_W)) {
             if (ball_y - ball_radius > 0) {
+                ball_y -= ball_speed_y;
             }
         }
-
-        // S tuşuna basıldığında aşağı hareket
+       
         if (IsKeyDown(KEY_S)) {
             if (ball_y + ball_radius < SCREEN_HEIGHT) {
                 ball_y += ball_speed_y;
             }
         }
 
-        // D tuşuna basıldığında sağa hareket
+       
         if (IsKeyDown(KEY_D)) {
             if (ball_x + ball_radius < SCREEN_WIDTH) { 
                 ball_x += ball_speed_x;
             }
         }
 
-        // A tuşuna basıldığında sola hareket
         if (IsKeyDown(KEY_A)) {
             if (ball_x - ball_radius > 0) { 
                 ball_x -= ball_speed_x;
             }
         }
+    }
+
+    void Draw() {
+        DrawCircle(ball_x, ball_y, ball_radius, BLUE);
+    }
+};
+
+
+class Dusman{
+
+
+    public:
+    int ball_x;
+    int ball_y;
+    int ball_speed_x;
+    int ball_speed_y;
+    int ball_radius;
+
+    Dusman(int startX, int startY, int startSpeedX, int startSpeedY, int ballRadius) 
+        : ball_x(startX), ball_y(startY), ball_speed_x(startSpeedX), ball_speed_y(startSpeedY), ball_radius(ballRadius) {}
+
+    void Update(int SCREEN_WIDTH, int SCREEN_HEIGHT) {
+        if (IsKeyDown(KEY_U)) {
+            if (ball_y - ball_radius > 0) {
+                ball_y -= ball_speed_y;
+            }
+        }
+       
+        if (IsKeyDown(KEY_K)) {
+            if (ball_y + ball_radius < SCREEN_HEIGHT) {
+                ball_y += ball_speed_y;
+            }
+        }
+
+       
+        if (IsKeyDown(KEY_L)) {
+            if (ball_x + ball_radius < SCREEN_WIDTH) { 
+                ball_x += ball_speed_x;
+            }
+        }
+
+        if (IsKeyDown(KEY_H)) {
+            if (ball_x - ball_radius > 0) { 
+                ball_x -= ball_speed_x;
+            }
+        }
+    }
+
+    void Draw() {
+        DrawCircle(ball_x, ball_y, ball_radius, RED);
+    }
+
+
+
+};
+
+
+int main() {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+    Ball ball(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 5, 5, 15);
+    Dusman dusman(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 5, 5, 15);
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        dusman.Update(SCREEN_WIDTH, SCREEN_HEIGHT);
+        ball.Update(SCREEN_WIDTH, SCREEN_HEIGHT);
 
         BeginDrawing();
         ClearBackground(GRAY);
-        DrawCircle(ball_x, ball_y, ball_radius, BLUE);
+        ball.Draw();
+        dusman.Draw();
         EndDrawing();
     }
 
