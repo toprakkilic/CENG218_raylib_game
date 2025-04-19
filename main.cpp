f837deb8ac46e7818de7d0052f62f915ced4d5eb
Toprak KILIÇ <toprakkilic23@gmail.com>
2025-04-19 13:09:03 +0300

    yuvarlağın ekran içerisinde hareket etmesi ve ekranın dışına çıkması engellendi

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int main () {
-
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
     int ball_x = 100;
@@ -18,26 +17,40 @@ int main () {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
     SetTargetFPS(60);
 
-    while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
+    while (WindowShouldClose() == false) {
+        // W tuşuna basıldığında yukarı hareket
+        if (IsKeyDown(KEY_W)) {
+            if (ball_y - ball_radius > 0) {
+            }
         }
 
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
+        // S tuşuna basıldığında aşağı hareket
+        if (IsKeyDown(KEY_S)) {
+            if (ball_y + ball_radius < SCREEN_HEIGHT) {
+                ball_y += ball_speed_y;
+            }
         }
-        
+
+        // D tuşuna basıldığında sağa hareket
+        if (IsKeyDown(KEY_D)) {
+            if (ball_x + ball_radius < SCREEN_WIDTH) { 
+                ball_x += ball_speed_x;
+            }
+        }
+
+        // A tuşuna basıldığında sola hareket
+        if (IsKeyDown(KEY_A)) {
+            if (ball_x - ball_radius > 0) { 
+                ball_x -= ball_speed_x;
+            }
+        }
+
         BeginDrawing();
-            ClearBackground(BLACK);
-            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+        ClearBackground(GRAY);
+        DrawCircle(ball_x, ball_y, ball_radius, BLUE);
         EndDrawing();
     }
 
     CloseWindow();
+    return 0;
 }
